c104f961eaa9de3aad9bb388d866b6617b8b1923
ManhCuong173 <daomanhcuong173@gmail.com>
2020-09-04 22:36:32 +0700

    Updates

---
+++ precache-manifest.568186107052c6a38b7e0769df25ae42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57698130bfa94aa8c371ff4ce0e4b37f",
+    "url": "/ReactJS_Staff-Management/index.html"
+  },
+  {
+    "revision": "ff0574152ffa57f0ee08",
+    "url": "/ReactJS_Staff-Management/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "e7e671aa90bb2613ef94",
+    "url": "/ReactJS_Staff-Management/static/css/main.8923a62a.chunk.css"
+  },
+  {
+    "revision": "ff0574152ffa57f0ee08",
+    "url": "/ReactJS_Staff-Management/static/js/2.26240625.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/ReactJS_Staff-Management/static/js/2.26240625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e671aa90bb2613ef94",
+    "url": "/ReactJS_Staff-Management/static/js/main.e7ad8227.chunk.js"
+  },
+  {
+    "revision": "041ff5e05bd9689fc91e",
+    "url": "/ReactJS_Staff-Management/static/js/runtime-main.9b22b330.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJS_Staff-Management/static/media/logo.5d5d9eef.svg"
+  }
+]);
